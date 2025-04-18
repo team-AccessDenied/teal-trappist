b932acea0797ad6e3d03f0b5ea5a17ddfc6fae7e
Aishani Sood <soodaishani6@gmail.com>
2025-04-19 04:02:14 +0530

    Final Site I think

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-    base: '/teal-trappist/',
-});
-
+  site: 'https://team-AccessDenied.github.io',
+})
